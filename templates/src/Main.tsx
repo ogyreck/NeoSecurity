8aa1b2b127ad15e931c3c4ad0cd7208c7b07e189
CameronKing18 <Gmansurov18@gmail.com>
2024-03-15 22:19:43 +0300

    fix commit front

---
+++ templates/src/Main.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { Sidebar } from './components/Sidebar';
+
+export const Main: React.FC = () => {
+  return (
+      <div>
+        <Sidebar/>
+      </div>
+  )
+};
